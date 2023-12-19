495558480aa31a1768c74a58858eb3cd41b6dff1
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-12-18 21:29:48 -0300

    ExcecoesPt1

--- TerceiroCod/Program.cs
+++ TerceiroCod/Program.cs
@@ -1,15 +1,71 @@
 ﻿using TerceiroCod.Models;
 using System.Globalization;
 
-//formatando valores monetários
-decimal valorMonetario = 1523.32M;
+//Execções genéricas e especificas
 
-Console.WriteLine($"{valorMonetario:c}");
-//:c é para formatar em formato de currency(moeda),
-// e ele retorna em reais porque as cofigurações do meu sistema está para o local que estou
-//para alterar segue abaixo o exemplo lembrando de colocar o using System.Globalization; no inicio
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-//para mais formatação de dados ver documentação
+try{
+     string [] linhas = File.ReadAllLines("Arquivos/d/arquivo_01.txt");
+    foreach(string linha in linhas){
+         Console.WriteLine(linha);
+     }
+}catch(FileNotFoundException ex){
+    Console.WriteLine($"Ocorreu uma exceção específica de arquivo: {ex.Message}");
+}
+catch(DirectoryNotFoundException ex){
+    Console.WriteLine($"Ocorreu uma exceção específica de diretório: {ex.Message}");
+}
+
+catch(Exception ex){//genérica
+     Console.WriteLine($"Ocorreu uma exceção genérica: {ex.Message}");
+} 
+finally{
+    Console.WriteLine("Programa executado");
+}
+//o bloco finally é executado independente de ter exceções
+
+
+
+
+
+
+
+
+
+
+
+//Exceções
+// try{
+//     string [] linhas = File.ReadAllLines("Arquivos/arquivo_01.txt");
+//     foreach(string linha in linhas){
+//         Console.WriteLine(linha);
+//     }
+// }catch(Exception ex){
+//     Console.WriteLine($"Ocorreu uma exceção: {ex.Message}");
+// }
+// // try - ele vai tentar executar o código
+// // catch(Exception ex) - sizeof tiver uma exceção ele vai armazená-la na variavel ex
+// // obs - sem o tratamento de exceções ele não vai executar nada depois que parar na exceção
+// Console.WriteLine("Com o tratamento ele executa o que vem depois da exceção");
+
+
+
+
+
+
+
+
+
+
+
+// //formatando valores monetários
+// decimal valorMonetario = 1523.32M;
+
+// Console.WriteLine($"{valorMonetario:c}");
+// //:c é para formatar em formato de currency(moeda),
+// // e ele retorna em reais porque as cofigurações do meu sistema está para o local que estou
+// //para alterar segue abaixo o exemplo lembrando de colocar o using System.Globalization; no inicio
+// Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+// //para mais formatação de dados ver documentação
 
 
 
